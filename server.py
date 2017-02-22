5be8b7ea23fe384ec45719ee60a23623be906797
Ivan Filimonov <snake73w@gmail.com>
2017-02-22 23:38:11 +0700

    put choosing color of backlog waiting time in template

--- server.py
+++ server.py
@@ -27,12 +27,6 @@ def get_score():
             .order_by(Order.created.asc()).first()
     backlog_waiting = round((datetime.utcnow() - first_unprocessed_order.created)\
             .total_seconds()/60)
-    if backlog_waiting < 8:
-        backlog_color = '#2ECC40' # green
-    elif backlog_waiting <= 30:
-        backlog_color = '#FFDC00' # yellow
-    else:
-        backlog_color = '#FF4136' # red
     backlog_count = session.query(Order).filter(Order.confirmed.is_(None)).count()
     date_utcnow = datetime.utcnow().date()
     orders_processed = session.query(Order)\
@@ -40,7 +34,6 @@ def get_score():
                     Order.confirmed < (date_utcnow + timedelta(days=1))).count()
 
     return jsonify(backlogWaitingTime=backlog_waiting,
-            backlogWaitingColor=backlog_color,
             backlogCount=backlog_count,
             ordersProcessed=orders_processed)
 
