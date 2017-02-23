32e7bdcb8d011ccfd448af19f9968813621a642b
Ivan Filimonov <snake73w@gmail.com>
2017-02-23 16:15:45 +0700

    escape broken linter 2/2

--- server.py
+++ server.py
@@ -9,10 +9,10 @@ from sqlalchemy.ext.automap import automap_base
 app = Flask(__name__)
 app.config.from_object('config')
 
-base = automap_base()
+Base = automap_base()
 engine = create_engine('postgres://score:Rysherat2@shopscore.devman.org:5432/shop')
-base.prepare(engine, reflect=True)
-order = base.classes.orders
+Base.prepare(engine, reflect=True)
+Order = Base.classes.orders
 session = Session(engine)
 
 
@@ -23,15 +23,15 @@ def score():
 
 @app.route('/get_score')
 def get_score():
-    first_unprocessed_order = session.query(order).filter(order.confirmed.is_(None))\
-            .order_by(order.created.asc()).first()
+    first_unprocessed_order = session.query(Order).filter(Order.confirmed.is_(None))\
+            .order_by(Order.created.asc()).first()
     backlog_waiting = round((datetime.utcnow() - first_unprocessed_order.created)\
             .total_seconds()/60)
-    backlog_count = session.query(order).filter(order.confirmed.is_(None)).count()
+    backlog_count = session.query(Order).filter(Order.confirmed.is_(None)).count()
     date_utcnow = datetime.utcnow().date()
-    orders_processed = session.query(order)\
-            .filter(order.confirmed > (date_utcnow - timedelta(days=1)),
-                    order.confirmed < (date_utcnow + timedelta(days=1))).count()
+    orders_processed = session.query(Order)\
+            .filter(Order.confirmed > (date_utcnow - timedelta(days=1)),
+                    Order.confirmed < (date_utcnow + timedelta(days=1))).count()
 
     return jsonify(backlogWaitingTime=backlog_waiting,
             backlogCount=backlog_count,
